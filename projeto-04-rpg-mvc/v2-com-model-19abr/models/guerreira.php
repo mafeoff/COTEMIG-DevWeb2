4048dc67691b20421790d4e0006b289fbe49f442
Mafe <contato.mf.13@gmail.com>
2023-04-19 10:00:00 -0300

    Add refactored RPG project with MVC

---
+++ projeto-04-rpg-mvc/v2-com-model-19abr/models/guerreira.php
@@ -0,0 +1,9 @@
+<?php
+    class Guerreira extends Personagem {
+
+        public function __construct($nickname) {
+            parent::__construct("Guerreira", "personagem-06.png", $nickname, 1, 0, 0);
+        }
+
+    }
+?>
